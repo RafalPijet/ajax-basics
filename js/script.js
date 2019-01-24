e1b5d875d8ac3a944a423cad9fe37599a3ef3641
Rafal Pijet <stronglopez@wp.pl>
2019-01-24 17:45:38 +0100

    Adding recommendation from task 16.4

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 "use strict";
 (function () {
+    var url = "http://api.icndb.com/jokes/random";
+    var button = document.getElementById("get-joke");
+    var paragraph = document.getElementById("joke");
 
+    button.addEventListener("click", function () {
+        getJoke();
+    });
 
+    function getJoke() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
 
+        xhr.addEventListener("load", function () {
+            var response = JSON.parse(xhr.response);
+            paragraph.innerText = response.value.joke + " (ID=" + response.value.id + ")";
+        });
+        xhr.send();
+    }
+
+    getJoke();
 })();
