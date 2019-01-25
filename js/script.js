71a4595e68b22bdccc7d58cf5e1f4ffc4e25fc3a
Rafal Pijet <stronglopez@wp.pl>
2019-01-25 11:22:19 +0100

    Adding recommendation from Oskar Master:)

--- js/script.js
+++ js/script.js
@@ -11,10 +11,12 @@
     function getJoke() {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url);
+        button.disabled = true;
 
         xhr.addEventListener("load", function () {
             var response = JSON.parse(xhr.response);
             paragraph.innerText = response.value.joke + " (ID=" + response.value.id + ")";
+            button.disabled = false;
         });
         xhr.send();
     }
