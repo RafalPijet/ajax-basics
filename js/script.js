b0aca54a43636e6bd82bfa8f70199b4be485947b
Rafal Pijet <stronglopez@wp.pl>
2019-01-26 15:55:38 +0100

    Adding new recommendation from Oskar Master :)

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
     var url = "http://api.icndb.com/jokes/random";
     var button = document.getElementById("get-joke");
     var paragraph = document.getElementById("joke");
+    var icon = document.querySelector("img");
+    var description = document.querySelector("#name");
 
     button.addEventListener("click", function () {
         getJoke();
@@ -12,11 +14,15 @@
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         button.disabled = true;
+        icon.classList.remove("display-off");
+        description.classList.add("display-off");
 
         xhr.addEventListener("load", function () {
             var response = JSON.parse(xhr.response);
             paragraph.innerText = response.value.joke + " (ID=" + response.value.id + ")";
             button.disabled = false;
+            icon.classList.add("display-off");
+            description.classList.remove("display-off");
         });
         xhr.send();
     }
